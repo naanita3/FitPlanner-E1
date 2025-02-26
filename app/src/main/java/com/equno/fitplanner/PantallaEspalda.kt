fea38b771191168bb0b71a4153d1d53b0813d17f
Omar <omaaramirez@gmail.com>
2025-02-26 15:18:33 -0600

    Pantallas de ejercicios de pecho y espalda

--- app/src/main/java/com/equno/fitplanner/PantallaEspalda.kt
+++ app/src/main/java/com/equno/fitplanner/PantallaEspalda.kt
@@ -1,39 +1,20 @@
 package com.equno.fitplanner
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ExplorarEjercicios : AppCompatActivity() {
+class PantallaEspalda : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_explorar_ejercicios)
+        setContentView(R.layout.activity_pantalla_espalda)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val imgbtn: ImageButton = findViewById(R.id.entrenamientopecho)
-
-        imgbtn.setOnClickListener {
-            val intent = Intent(this, PantallaPectoral::class.java)
-            startActivity(intent)
-        }
-        val imgbtn2 : ImageButton = findViewById(R.id.EjercitandoEspalda)
-
-        imgbtn.setOnClickListener {
-            val intent = Intent(this, PantallaEspalda::class.java)
-            startActivity(intent)
-        }
     }
-    }
-
-
-
 }
