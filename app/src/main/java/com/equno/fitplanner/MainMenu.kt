321f808ac9913a059ec6253359f107b0e68e2008
Omar <omaaramirez@gmail.com>
2025-02-26 13:48:39 -0600

    Menu principal funcionalidad 1

--- app/src/main/java/com/equno/fitplanner/MainMenu.kt
+++ app/src/main/java/com/equno/fitplanner/MainMenu.kt
@@ -1,6 +1,9 @@
 package com.equno.fitplanner
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +11,15 @@ class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainmenu)
+        val imgbtn : ImageButton = findViewById(R.id.entrenamientosP)
+
+        imgbtn.setOnClickListener {
+            val intent = Intent(this, ExplorarEjercicios::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
+
