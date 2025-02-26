5ef4dfa10261ec3b9389ead5567d0e5e95583165
naanita3 <142059522+naanita3@users.noreply.github.com>
2025-02-26 11:19:28 -0600

    Initial commit

---
+++ app/src/main/java/com/equno/fitplanner/MainMenu.kt
@@ -0,0 +1,12 @@
+package com.equno.fitplanner
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainmenu)
+    }
+}
