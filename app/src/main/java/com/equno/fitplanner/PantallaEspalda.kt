2f2641355c106fdd0f3bc6738bfadb5670ca7527
Omar <omaaramirez@gmail.com>
2025-03-06 13:49:52 -0600

    Plantilla ejercicios Sergio

--- app/src/main/java/com/equno/fitplanner/PantallaEspalda.kt
+++ app/src/main/java/com/equno/fitplanner/PantallaEspalda.kt
@@ -1,10 +1,13 @@
 package com.equno.fitplanner
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.equno.fitplanner.ESPALDA.ejercicio001
 
 class PantallaEspalda : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,12 @@ class PantallaEspalda : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val imgbtn: ImageButton = findViewById(R.id.jalonpecho)
+
+        imgbtn.setOnClickListener {
+            val intent = Intent(this, ejercicio001::class.java)
+            startActivity(intent)
         }
     }
 }
